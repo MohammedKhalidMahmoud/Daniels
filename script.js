07d881bf6fb21ed8fb38398baad7d126dfe70b02
mkhaled <mohammedkhalidmahmoud@gmail.com>
2025-03-23 22:13:29 +0200

    add major changes

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typing', {
+    strings: ["Larry Daniels.", "Developer.", "Designer."],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 500,
+    startDelay: 500,
+    loop: true,
+  });
